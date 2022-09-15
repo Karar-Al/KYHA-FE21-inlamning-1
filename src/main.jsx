ff163816d925453b86b7d07f2ee0d8d3459bd009
Karar-Al <karar.alremahy@student.kyh.se>
2022-09-15 14:32:30 +0200

    fix: Split CSS files.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app'
 
-import 'normalize.css' // Normalizes CSS.
-import 'minireset.css' // Gives us some nice CSS defaults.
-
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
